105b344083eeaf4e0889cff79d1d1a9ff098e2bb
Neek Sandhu <neek.sandhu@outlook.com>
2023-06-09 17:15:21 -0700

    Fix typescript issues

--- packages/destination-actions/src/destinations/acoustic/Utility/tablemaintutilities.ts
+++ packages/destination-actions/src/destinations/acoustic/Utility/tablemaintutilities.ts
@@ -6,7 +6,7 @@ import {
 } from '@segment/actions-core'
 import { RequestClient } from '@segment/actions-core'
 import { Settings } from '../generated-types'
-import { AuthTokens } from '@segment/actions-core/src/destination-kit/parse-settings'
+import { AuthTokens } from '@segment/actions-core/destination-kit/parse-settings'
 
 // export let eventTableListId = ''
 export interface accessResp {
